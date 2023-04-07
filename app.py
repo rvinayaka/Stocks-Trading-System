a0549069c0c6539d04f151a6cacc3ab662767b51
rvinayaka <r.vinayak@banyancloud.io>
2023-04-07 15:21:48 +0530

    requirements added

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask, request, jsonify
 from conn import connection
-from settings import logger
+from settings import logger, handle_exceptions
 import psycopg2
 
 app = Flask(__name__)
@@ -26,30 +26,6 @@ app = Flask(__name__)
 
 
 
-
-def handle_exceptions(func):
-    def wrapper(*args, **kwargs):
-        try:
-            return func(*args, **kwargs)
-        except psycopg2.Error as error:
-            conn = kwargs.get('conn')
-            if conn:
-                conn.rollback()
-            logger(__name__).error(f"Error occurred: {error}")
-            return jsonify({"message": f"Error occurred: {error}"})
-        except Exception as error:
-            logger(__name__).error(f"Error occurred: {error}")
-            return jsonify({"message": f"Error occurred: {error}"})
-        finally:
-            conn = kwargs.get("conn")
-            cur = kwargs.get("cur")
-            # close the database connection
-            if conn:
-                conn.close()
-            if cur:
-                cur.close()
-            logger(__name__).warning("Closing the connection")
-    return wrapper
 
 
 @app.route("/stocks", methods=["POST"])             # CREATE a stock profile
